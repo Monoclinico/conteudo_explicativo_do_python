1d148fe90df4111ab602e7568e7470d1fe55039a
Matheus Sanches <matheussanchesleme@gmail.com>
2019-11-07 17:55:20 -0300

    Arquivo sobre datas no Python criado

--- datas.py
+++ datas.py
@@ -1,12 +1,3 @@
-class Geladeira: 
-  def __init__(self): 
-    self.temperatura = -20 
-    self.minimo = -2 
-    self.maximo = 2 
-  def regularTemperatura(self): 
-    if self.temperatura < self.minimo: 
-      self.temperatura -= 1 
-    elif self.temperatura < self.minimo: 
-      self.temperatura += 1 
-  def mostrarTemperatura(self): 
-    print('Atual: {}.'.format(self.temperatura)) 
+#Uma data no Python não é um tipo de dado próprio, 
+# mas podemos importar um módulo chamado datetime 
+# para trabalhar com datas como objetos de data.
