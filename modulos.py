2f280de5b10e141e40cba94112d63f98cfd3d499
Matheus Sanches <matheussanchesleme@gmail.com>
2019-12-10 19:25:26 -0300

    Exemplo de importação utilizando a funcao __import__().

--- modulos.py
+++ modulos.py
@@ -1,10 +1,12 @@
 #importar todo o modulo
+#lento
 import platform
 
 print(platform.python_version())
 print(platform.system())
 
 #importar uma parte especifica
+#mais rapido
 from math import sqrt
 print(sqrt(4)) #2
 
@@ -13,7 +15,10 @@ from math import pow as potencia
 print(potencia(8,2)) #64
 
 #importar todas as partes diretamente
+#mais lento
 from random import *
 print(choice([5,6,4]))
 
-
+#importar utilizando a funcao __import__()
+tempo = __import__('time', globals(), locals(), [])
+tempo.sleep(5)
